83c0e03a5b300b04eaa8b2f3c23b3805c62d2dab
SaraHamsassia <hamsassi@outlook.fr>
2024-01-09 14:26:30 +0100

    first commit

---
+++ src/test/java/com/users/usersmicroservice/UsersMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.usersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
